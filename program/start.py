9c793552887cb36b4bec2d0cc6b01ea3f670f670
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-03-19 19:22:28 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -92,7 +92,7 @@ async def start_(c: Client, message: Message):
 
 ☞ I Cᴀɴ Pʟᴀʏ Mᴜsɪᴄ Iɴ Yᴏᴜʀ Gʀᴏᴜᴘ.
 
-» Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ :- [🍻ᯓ𝐌 𝚫 𝐇 𝐈ˢ ͪᵘ ᷜˡ ⷶ™🇮🇳](t.me/official_pro_xD).
+» Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ :- [AADITYA](t.me/SEXYAADITYA).
 """,
         reply_markup=InlineKeyboardMarkup(
             [
