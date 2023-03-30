cab939f6a70db01f1dc3277e08b79c4ea50e0e84
vipinofficialxd <120773322+vipinofficialxd@users.noreply.github.com>
2023-03-30 15:24:04 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -92,7 +92,7 @@ async def start_(c: Client, message: Message):
 
 ☞ I Cᴀɴ Pʟᴀʏ Mᴜsɪᴄ Iɴ Yᴏᴜʀ Gʀᴏᴜᴘ.
 
-» Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ :- [VIPIN](t.me/SEXYVIPIN).
+» Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ :- [KIARA](t.me/SEXYAADITYA).
 """,
         reply_markup=InlineKeyboardMarkup(
             [
